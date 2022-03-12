4ccf3b34f57c85359237488b661a8f73258c523d
andreagsilva <andreasilva.and@gmail.com>
2022-03-12 19:30:36 -0300

    first commit

---
+++ Primeiroprograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		Livros livros = new Livros();
+		Gato gato = new Gato();
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+}	
+
+class Livros{
+	private String nome;
+	private String npag;
+}
